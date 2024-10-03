0ce297c043dd18b92328034a05486465d3a86a5c
felippe.rodrigues-circini <felippe.rodrigues@circini.co.nz>
2024-10-04 11:35:07 +1300

    Adapting from SiS to Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your Smoothie")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options")
 my_column = my_dataframe.select(col('FRUIT_NAME'))
 
